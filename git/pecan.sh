552e98d8fd86abec49dfacc3fe3a9e36cd95d0f0
Johnny C. Lam <jlam88@gmail.com>
2012-11-20 16:38:04 -0500

    Git shell scripts depend on bash features.

--- git/pecan.sh
+++ git/pecan.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PECAN_PKGNAME="git-1.8.0"
+PECAN_PKGNAME="git-1.8.0+1"
 
 pecan_description="The stupid content tracker"
 
@@ -14,7 +14,8 @@ pecan_prereq_lib="${pecan_prereq_lib} >= libiconv-1.14"
 pecan_prereq_lib="${pecan_prereq_lib} >= openssl-1.0.1c"
 pecan_prereq_lib="${pecan_prereq_lib} >= pcre-5.8"
 pecan_prereq_lib="${pecan_prereq_lib} >= zlib-1.2.7"
-pecan_prereq_pkg=">= perl-5.8"
+pecan_prereq_pkg="* bash"
+pecan_prereq_pkg="${pecan_prereq_pkg} >= perl-5.8"
 pecan_prereq_pkg="${pecan_prereq_pkg} >= python-2.6"
 
 if [ -f ./pecan.subr ]; then
@@ -32,6 +33,7 @@ configure_args="${configure_args} --with-expat"
 configure_args="${configure_args} --with-iconv"
 configure_args="${configure_args} --with-perl=${PECAN_TARGET}/bin/perl"
 configure_args="${configure_args} --with-python=${PECAN_TARGET}/bin/python"
+configure_args="${configure_args} --with-shell=${PECAN_TARGET}/bin/bash"
 configure_args="${configure_args} --without-tcltk"
 
 pecan_gnu_configure_args="${pecan_gnu_configure_args} ${configure_args}"
