0e3b3184ac02f912381709969570522a21387bc7
Johnny C. Lam <jlam88@gmail.com>
2012-11-27 11:19:39 -0500

    Add new pecan package: patch-2.7.1.
    Also make `patch' an automatic build prereq for packages that have
    patch files.

---
+++ patch/pecan.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+PECAN_PKGNAME="patch-2.7.1"
+
+pecan_description="GNU patch utility to apply diffs"
+
+if [ -f ./pecan.subr ]; then
+	. ./pecan.subr
+elif [ -f ../pecan/pecan.subr ]; then
+	. ../pecan/pecan.subr
+else
+	exit 1
+fi
+
+pecan_gnu_configure_args="${pecan_gnu_configure_args} --disable-merge"
+
+pecan_test_style=make
+
+pecan_post_stage()
+{
+	echo "exclude lib/charset.alias" >> "${pecan_stage_encapinfo}"
+	cp "${pecan_srcdir}/COPYING" "${pecan_stagedir}"
+}
+
+pecan_main "$@"
