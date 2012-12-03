5142fdc484caf29029bf1c3eaf149ba92920b22f
Johnny C. Lam <jlam88@gmail.com>
2012-12-03 01:35:17 -0500

    Add new pecan package: gmp-5.0.5

---
+++ gmp/pecan.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+PECAN_PKGNAME="gmp-5.0.5"
+
+pecan_description="GNU multiple precision arithmetic library"
+
+pecan_extract_suffix=".tar.bz2"
+
+if [ -f ./pecan.subr ]; then
+	. ./pecan.subr
+elif [ -f ../pecan/pecan.subr ]; then
+	. ../pecan/pecan.subr
+else
+	exit 1
+fi
+
+pecan_test_style=make
+
+pecan_post_stage()
+{
+	echo "exclude share/info/dir" >> "${pecan_stage_encapinfo}"
+	cp "${pecan_srcdir}/COPYING" "${pecan_stagedir}"
+	cp "${pecan_srcdir}/COPYING.LIB" "${pecan_stagedir}"
+	cp "${pecan_srcdir}/README" "${pecan_stagedir}/README.gmp"
+}
+
+pecan_main "$@"
