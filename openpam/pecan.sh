b1a85581bd31c2b1642bea4afed6009629d81054
Johnny C. Lam <jlam88@gmail.com>
2012-12-03 17:03:45 -0500

    Add new pecan package: openpam-20120526

---
+++ openpam/pecan.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+PECAN_PKGNAME="openpam-20120526"
+
+pecan_description="Pluggable authentication mechanism (PAM) library"
+
+if [ -f ./pecan.subr ]; then
+	. ./pecan.subr
+elif [ -f ../pecan/pecan.subr ]; then
+	. ../pecan/pecan.subr
+else
+	exit 1
+fi
+
+# Pass in additional CPPFLAGS to openpam Makefiles.
+AM_CPPFLAGS="-DOPENPAM_COMMON_MODULE_DIR=\\\"${PECAN_TARGET}/lib/security\\\""
+AM_CPPFLAGS="${AM_CPPFLAGS} -DSYSCONFDIR=\\\"${pecan_etcdir}\\\""
+export AM_CPPFLAGS
+
+pecan_post_stage()
+{
+	cp "${pecan_srcdir}/LICENSE" "${pecan_stagedir}"
+}
+
+pecan_main "$@"
