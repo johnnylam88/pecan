9a56bfd04ef645a1b8f737d57524ad788d5144d9
Johnny C. Lam <jlam88@gmail.com>
2012-12-06 17:20:56 -0500

    Add new pecan package: openldap-client-2.4.33.

---
+++ openldap-client/pecan.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+openldap_version=2.4.33
+PECAN_PKGNAME="openldap-client-${openldap_version}"
+
+pecan_description="Lightweight Directory Access Protocol (LDAP) client & libraries"
+
+pecan_prereq_lib=">= cyrus-sasl-2.1.26"
+pecan_prereq_lib="${pecan_prereq_lib} >= openssl-1.0.1c"
+
+pecan_extract_file="openldap-${openldap_version}.tgz"
+pecan_extract_suffix=".tgz"
+
+if [ -f ./pecan.subr ]; then
+	. ./pecan.subr
+elif [ -f ../pecan/pecan.subr ]; then
+	. ../pecan/pecan.subr
+else
+	exit 1
+fi
+
+# Don't build the slapd(8) server.
+configure_args="--disable-slapd"
+
+configure_args="${configure_args} --enable-dynamic"
+configure_args="${configure_args} --with-threads"
+configure_args="${configure_args} --with-tls=openssl"
+configure_args="${configure_args} --without-fetch"
+
+pecan_gnu_configure_args="${pecan_gnu_configure_args} ${configure_args}"
+
+pecan_post_stage()
+{
+	cp "${pecan_srcdir}/COPYRIGHT" "${pecan_stagedir}"
+	cp "${pecan_srcdir}/LICENSE" "${pecan_stagedir}"
+}
+
+openldap_exampledir="${pecan_pkgdir}/share/examples/openldap"
+pecan_install_args="${pecan_install_args} sysconfdir=${openldap_exampledir}"
+
+pecan_post_install()
+{
+	rm -f "${openldap_exampledir}"/*.default
+}
+
+pecan_main "$@"
