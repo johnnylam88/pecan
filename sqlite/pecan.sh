bdcc1c426a22242c37ef4c051cfd3e77ba97e3e9
Johnny C. Lam <jlam88@gmail.com>
2012-12-21 14:24:27 -0500

    Update pecan package: sqlite-3.7.15.1.

--- sqlite/pecan.sh
+++ sqlite/pecan.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-PECAN_PKGNAME="sqlite-3.7.14.1+1"
+PECAN_PKGNAME="sqlite-3.7.15.1"
 
 pecan_description="Server-less, transactional SQL database engine"
 
-pecan_extract_file="sqlite-autoconf-3071401.tar.gz"
+pecan_extract_file="sqlite-autoconf-3071501.tar.gz"
 
 pecan_prereq_lib=">= ncurses-5.0"
 pecan_prereq_lib="${pecan_prereq_lib} >= readline-6.2"
