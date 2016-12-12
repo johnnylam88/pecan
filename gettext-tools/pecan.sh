0a9c071d27cf93422a501dad9d48905d9906296a
Johnny C. Lam <jlam88@gmail.com>
2016-12-12 03:49:03 +0000

    Update ``gettext-tools'' to 0.19.8.1.
    Removes expat and ncurses as prerequisites.

--- gettext-tools/pecan.sh
+++ gettext-tools/pecan.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-PECAN_PKGNAME="gettext-tools-0.18.1.1"
+PECAN_PKGNAME="gettext-tools-0.19.8.1"
 
 pecan_description="GNU internationalization & localization tools"
 
-pecan_fetch_file="gettext-0.18.1.1.tar.gz"
+pecan_fetch_file="gettext-0.19.8.1.tar.gz"
 pecan_fetch_url="http://ftp.gnu.org/gnu/gettext/%FILE%"
 
-pecan_prereq_lib=">= expat-2.1.0"
-pecan_prereq_lib="${pecan_prereq_lib} >= gettext-runtime-0.18.1.1"
+pecan_prereq_lib=">= gettext-runtime-0.19.8.1"
 pecan_prereq_lib="${pecan_prereq_lib} >= libiconv-1.14"
-pecan_prereq_lib="${pecan_prereq_lib} >= ncurses-5.0"
 
 if [ -f ./pecan/pecan.subr ]; then
 	. ./pecan/pecan.subr
@@ -21,9 +19,14 @@ else
 fi
 
 pecan_configure_style="gnu"
+pecan_configure_args="${pecan_configure_args} --disable-curses"
 pecan_configure_args="${pecan_configure_args} --disable-java"
+pecan_configure_args="${pecan_configure_args} --without-emacs"
 pecan_configure_args="${pecan_configure_args} --without-included-gettext"
 pecan_configure_srcdir="${pecan_srcdir}/${pecan_pkgname}"
+
+# All gettext packages put their documentation in a common directory.
+pecan_docdir=${pecan_pkgdir}/share/doc/gettext
 
 pecan_post_stage()
 {
