06aa533342704b6b65211b57f55b1177a9916a9b
Johnny C. Lam <jlam88@gmail.com>
2016-12-12 16:36:40 +0000

    Update ``pkg-config'' to 0.29.1.
    This version is less aggressive with removing duplicate linker flags.

--- pkg-config/pecan.sh
+++ pkg-config/pecan.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PECAN_PKGNAME="pkg-config-0.27.1"
+PECAN_PKGNAME="pkg-config-0.29.1"
 
 pecan_description="Utility that details how to compile and link against a library"
 
@@ -14,7 +14,8 @@ else
 	exit 1
 fi
 
-configure_args="--with-internal-glib"
+configure_args="--disable-host-tool"
+configure_args="${configure_args} --with-internal-glib"
 configure_args="${configure_args} --with-pc-path=${PECAN_TARGET}/lib/pkgconfig"
 
 pecan_configure_style="gnu"
